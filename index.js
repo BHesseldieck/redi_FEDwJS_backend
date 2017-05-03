b70fe05795e7668c589510c0a32bb724994ee0dc
Ben Hesseldieck <b.hesseldieck@gmail.com>
2017-05-03 20:34:39 +0200

    dummy route complete

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
+const router = require('./routes.js');
 
 const port = process.env.PORT || 1337;
 
 require('./db/config.js');
 require('./middleware.js')(app);
-require('./routes.js')(app);
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
--- routes.js
+++ routes.js
@@ -1,5 +1,10 @@
-// require db controllers
+const express = require('express');
 
-module.exports = (app) => {
-  
-}
+// require db controllers
+const user = require('./db/controllers/user.js');
+
+const router = express.Router();
+
+router.get('/user', (req, res) => { res.send("Hello")});
+
+module.exports = router;
