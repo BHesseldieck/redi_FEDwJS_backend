d4dc1a362cf057bfa8bffc23276d4ce3b346278b
Ben Hesseldieck <b.hesseldieck@gmail.com>
2017-05-03 20:41:06 +0200

    added wildcart route

--- routes.js
+++ routes.js
@@ -6,5 +6,6 @@ const user = require('./db/controllers/user.js');
 const router = express.Router();
 
 router.get('/user', (req, res) => { res.send("Hello")});
+router.get('/*', (req,res) => { res.status(404).send('Welcome to the ReDI School API. Unfortunately is the sub-page you requested non-existent!'); })
 
 module.exports = router;
