3ff08b8168130545afe4c33d88ca7afb85e190c2
Ben Hesseldieck <b.hesseldieck@gmail.com>
2017-05-03 19:54:47 +0200

    added middleware and routing file

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 const port = process.env.PORT || 1337;
 
+require('./db/config.js');
+require('./middleware.js')(app);
+require('./routes.js')(app);
+
 app.listen(port, () => {
   console.log(`Server listening on port: ${port}`);
 });
