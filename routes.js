7568eb7e1bacf81f1346d7b165feb4f8225422ce
Ben Hesseldieck <b.hesseldieck@gmail.com>
2017-05-12 21:09:26 +0200

    create user via post complete

--- routes.js
+++ routes.js
@@ -5,10 +5,10 @@ const userCtrl = require('./db/controllers/userCtrl.js');
 
 const router = express.Router();
 
-router.get('/user', userCtrl.fetchUsers);
-router.post('/newuser', userCtrl.createUser);
-router.delete('/deleteuser', userCtrl.deleteUser);
-router.post('/updateuser', userCtrl.updateUser);
+router.get('/api/user', userCtrl.fetchUsers);
+router.post('/api/newuser', userCtrl.createUser);
+router.delete('/api/deleteuser', userCtrl.deleteUser);
+router.post('/api/updateuser', userCtrl.updateUser);
 router.get('/*', (req,res) => { res.status(404).send('Welcome to the ReDI School API. Unfortunately is the sub-page you requested non-existent!'); })
 
 module.exports = router;
