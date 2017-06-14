e65ac2156a31633f1c0c99ffdfd897b9070c2c44
Ben Hesseldieck <b.hesseldieck@gmail.com>
2017-06-14 15:28:06 +0200

    added tests and CORS

--- middleware.js
+++ middleware.js
@@ -3,4 +3,11 @@ const bodyparser = require('body-parser');
 module.exports = (app) => {
   app.use(bodyparser.json());
   app.use(bodyparser.urlencoded({ extended: true }));
+  app.use((req, res, next) => {
+    res.header('access-control-allow-origin', '*');
+    res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('access-control-allow-headers', 'content-type, accept');
+    res.header('access-control-max-age', 10);
+    next();
+  });
 };
